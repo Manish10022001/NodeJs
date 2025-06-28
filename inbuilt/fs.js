47e4a67e5e60ecd910822e690297830db11cea72
Manish10022001 <manishshirsat2850@gmail.com>
2025-06-28 14:37:58 +0530

    first commit

--- inbuilt/fs.js
+++ inbuilt/fs.js
@@ -1,14 +1,16 @@
-let fs = require('fs');
-fs.writeFile('txt.txt',"this is my data of node",(err)=>{
-    if(err) throw err;
-    console.log("Task Done");
-    fs.writeFile('txt1.txt', 'this is my data of node inside anoter fs function',(err)=>{
-        if(err) throw err;
-        console.log('Task2 Done')
-    })
-})
+let fs = require("fs");
+fs.writeFile("myCode.txt", "this is my data of node", (err) => {
+  if (err) throw err;
+  console.log("Task Done");
+  // fs.writeFile('txt1.txt', 'this is my data of node inside anoter fs function',(err)=>{
+  //     if(err) throw err;
+  //     console.log('Task2 Done') we write writeFile method inside the callback function if we wan it to execute after the first one is executed.
+  //})
+});
 
 // fs.writeFile('txt3',"this is my data of node in parallel fs function",(err)=>{
 //     if(err) throw err;
 //     console.log('Task3 done')
 // })
+
+//drawback of writeFile: it overwrite the file, so to overcome we use appendFile
