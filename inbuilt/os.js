588ed2f903035aa2006e52c3a44ffecac5375fc4
Manish10022001 <manishshirsat2850@gmail.com>
2025-06-28 14:43:15 +0530

    made changes

--- inbuilt/os.js
+++ inbuilt/os.js
@@ -3,4 +3,4 @@ console.log(os.platform()); //os
 console.log(os.arch());     //os architecture
 console.log(os.cpus().length); //cpus numbers
 console.log(os.freemem());    //free memory
-console.log(os.uptime());   //os uptime
+console.log(os.uptime());   //os uptimedf
