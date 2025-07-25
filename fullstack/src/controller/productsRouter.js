74e6f2abf6deac3b7195978974671259ef666a8f
Manish10022001 <manishshirsat2850@gmail.com>
2025-07-25 17:19:16 +0530

    updated to get product details by category

--- fullstack/src/controller/productsRouter.js
+++ fullstack/src/controller/productsRouter.js
@@ -28,7 +28,38 @@ function router(menu) {
     });
     //res.render("products", { title: "Products Page", data: products, menu }); //this is the same `menu` from App.js
   });
-~
+
+  productRouter.route('/category/:id').get(function(req,res){
+    //to receive this id
+    //let id = req.params.id;
+    //if both sides of = have same variable in this case id is . we use short hand syntax
+    let {id} = req.params;
+    //now mongo db queries from above
+    mongodb.connect(url, function (err, dc) {
+      if (err) {
+        res.status(500).send("Error while connecting");
+      } else {
+        let dbObj = dc.db("nodedb");
+        dbObj
+          .collection("products")
+          .find({category_id:Number(id)})
+          .toArray(function (err, result) {
+            if (err) {
+              res.status(203).send("Error while fetching");
+            } else {
+              res.render("products", {
+                title: "Products Page",
+                data: result,
+                menu,
+              });
+            }
+            dc.close();
+          });
+      }
+    });
+    //res.send(id);
+  })
+
   productRouter.route("/details").get((req, res) => {
     res.send("Product Details");
   });
