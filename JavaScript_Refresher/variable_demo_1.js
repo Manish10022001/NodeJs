95c4abb6fa7249dbe5031c93309b11e2ca9b017c
Manish10022001 <manishshirsat2850@gmail.com>
2025-09-27 11:42:03 +0530

    variable demo

---
+++ JavaScript_Refresher/variable_demo_1.js
@@ -0,0 +1,18 @@
+var name = "Manish";
+let age = 34;
+const add = "Akola"
+
+name = "manish";
+age =31;
+//add = "nagpur";  reassignment for const not possible
+console.log(`my name is ${name}, I am ${age} years old, and I am from ${add}`);
+
+var a;
+let b;
+const c= 20;
+console.log(a,b,c);
+
+var a = 12;
+//let b =23;    -> can't redeclare
+//const c =28;  -> can't redeclare
+console.log(a,b,c);
