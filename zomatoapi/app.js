223766db599affbb76d6757ea1a00019d185a6f0
Manish10022001 <manishshirsat2850@gmail.com>
2025-11-02 21:10:32 +0530

    created routes for page 1

--- zomatoapi/app.js
+++ zomatoapi/app.js
@@ -15,9 +15,54 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
+let authKey = process.env.AuthKey;
+
+function auth(key) {
+  if (key == authKey) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 //get heart beat : to check if api calls are working properly
 app.get("/", (req, res) => {
   res.status(200).send("Health Ok");
+});
+//we do not want everywon to acces this api so we give auth key so without it on one can acces the api
+app.get("/location", async (req, res) => {
+  // let key = req.header("x-basic-token"); //used header because it is not browser generated
+  // if (auth(key)) {
+
+  const data = await db.collection("location").find({}).toArray();
+  res.status(200).send(data);
+});
+
+//getting restaurant wrt to citties and menuType
+app.get("/restaurant", async (req, res) => {
+  let query = {};
+  let stateId = Number(req.query.stateId);
+  let mealId = Number(req.query.mealId);
+
+  if (stateId && mealId) {
+    query = { state_id: stateId, "mealTypes.mealtype_id": mealId };
+  } else if (stateId) {
+    query = { state_id: stateId };
+  } else if (mealId) {
+    query = { "mealTypes.mealtype_id": mealId };
+  } else {
+    query = {};
+  }
+
+  const data = await db.collection("restaurants").find(query).toArray();
+  res.status(200).send(data);
+});
+
+app.get("/meals", async (req, res) => {
+  // let query = {};
+
+  const data = await db.collection("mealType").find({}).toArray();
+  res.status(200).send(data);
 });
 
 async function connectDb() {
@@ -38,3 +83,22 @@ connectDb();
 //         console.log("Running on port "+ port)
 //     })
 // })
+
+//getting list of restaurants
+// app.get("/restaurant", async (req, res) => {
+//   const data = await db.collection("restaurants").find().toArray();
+//   res.status(200).send(data);
+// });
+
+//getting restaurant list wrt cities
+// app.get("/restaurant", async (req, res) => {
+//   let query = {};
+//   let stateId = Number(req.query.stateId);
+//   if (stateId) {
+//     query = { state_id: stateId };
+//   } else {
+//     query = {};
+//   }
+//   const data = await db.collection("restaurants").find(query).toArray();
+//   res.status(200).send(data);
+// });
